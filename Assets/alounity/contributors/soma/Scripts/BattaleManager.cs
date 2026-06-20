abcffb1fb4b8233f31aec7db0516c442571d4076
chebuo <bob.chebuo@gmail.com>
2026-06-20 15:23:29 +0900

    [add] タイムアップ&カウントダウン&仮:死んだときの処理

--- Assets/alounity/contributors/soma/Scripts/BattaleManager.cs
+++ Assets/alounity/contributors/soma/Scripts/BattaleManager.cs
@@ -11,7 +11,9 @@ namespace FishRumble
         [Header("準備パラメータ")]
         [SerializeField] GameObject[] playerLocations;
         [SerializeField] GameObject playerPrefab;
+        [SerializeField] int countdownTimeMs;
         [SerializeField] int battleTimeMs;
+        [SerializeField] int DeadTimeMs;
         int[] playersLives;
         int currentTime;
         int winningTeamID;
@@ -27,19 +29,48 @@ namespace FishRumble
             //バトル準備
             PlayersSpawn();
 
-            //バトル開始
-            await Timer(battleTimeMs);
+            await Countdown();
 
-            //
+            //バトル開始
+            await StartCountdown();
+
+            //バトル終了
+            await TimeUp();
 
         }
 
         void PlayersSpawn()
         {
-            for(int i = 0; i >= AppManager.Instance.playerCount; i++)
+            for(int i = 0; i < AppManager.Instance.playerCount; i++)
             {
                 Instantiate (playerPrefab, playerLocations[i].transform.position, playerLocations[i].transform.rotation);
             }
+        }
+
+        async UniTask DeadCoolTime(PlayerState currentState,int teamID)
+        {
+            for(int i = 0; i < players.Length; i++)
+            {
+                 if (PlayerState.Dead == currentState&&teamID==i)
+                {
+                    await Timer(DeadTimeMs);
+                }
+            }
+        }
+
+        async UniTask TimeUp()
+        {
+            FinishGame();
+        }
+
+        void FinishGame()
+        {
+            
+        }
+
+        async UniTask Countdown()
+        {
+            await Timer(countdownTimeMs);
         }
 
         async UniTask StartCountdown()
@@ -47,16 +78,18 @@ namespace FishRumble
             await Timer(battleTimeMs);
         }
 
+        
+
         async UniTask Timer(int Ms)
         {
             currentTime = Ms;
-
-            for(int i = 0; i >= Ms; i++)
+            for(int i = 0; i < Ms; i++)
             {
                 await UniTask.Delay(1);
                 currentTime -= 1;
+                Debug.Log(currentTime);
             }
-
+            
             return;
         }
     }
