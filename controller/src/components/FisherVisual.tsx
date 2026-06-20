6fe3ff0bbf2e099ab63b9742371fff4cb4ab9a67
RiTa-23 <rita.tifrt@gmail.com>
2026-06-21 06:03:25 +0900

    fix: 漕ぎアニメーションの閾値を50に変更

--- controller/src/components/FisherVisual.tsx
+++ controller/src/components/FisherVisual.tsx
@@ -9,7 +9,7 @@ interface FisherVisualProps {
   team: Team;
 }
 
-const SWING_THRESHOLD = 100;
+const SWING_THRESHOLD = 50;
 const POWER_HOLD_DURATION = 800;
 
 type RodState = "idle" | "casting" | "reeling";
