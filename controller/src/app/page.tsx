dd7f537d67a58a3c7b51a26216238a902cc2f718
RiTa-23 <rita.tifrt@gmail.com>
2026-06-20 12:55:01 +0900

    feat: ルームID入力時の文字数を6文字に制限

--- controller/src/app/page.tsx
+++ controller/src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRoomId(e.target.value.toUpperCase().replace(/[^A-Z2-9]/g, ""));
+    setRoomId(e.target.value.toUpperCase().replace(/[^A-Z2-9]/g, "").slice(0, 6));
     setError("");
   };
 
