dbabc61705da737824723c8ea317964df7b5bfd2
chebuo <bob.chebuo@gmail.com>
2026-06-21 13:59:22 +0900

    [add] 石たち

--- Assets/alounity/contributors/soma/Scripts/BattaleManager.cs
+++ Assets/alounity/contributors/soma/Scripts/BattaleManager.cs
@@ -41,6 +41,7 @@ namespace FishRumble
         async UniTask EnterSettingsScreen()
         {
             await UniTask.WaitUntil(() => Input.GetKey(KeyCode.Space));
+            Debug.Log("<Color=red>tomato</Color>");
             QRPanel.SetActive(false);
         }
 
